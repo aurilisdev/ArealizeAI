652d2877de71095c3e80b99ba5fd997e4a4769e8
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2021-10-29 17:37:24 +0200

    Add parse_json

--- main/main.py
+++ main/main.py
@@ -1,1 +1,10 @@
-print("Hei")
+import numpy as np
+import json
+
+def parse_json(filepath):
+    with open(filepath, 'r') as rawinput:
+        data = json.load(rawinput)
+        floor_plan = data['planBoundary']
+        room_dict = data['rooms']
+    # return  ( FloorPlanCoordinates, RoomDictionary )
+    return floor_plan, room_dict
